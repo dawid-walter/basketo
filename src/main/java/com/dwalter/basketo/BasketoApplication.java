d4eeffd16c4fb289c4722119975ad4ebbf6a0fa5
Dawid <davedohc@aim.com>
2026-01-29 19:27:38 +0100

    init commit

---
+++ src/main/java/com/dwalter/basketo/BasketoApplication.java
@@ -0,0 +1,13 @@
+package com.dwalter.basketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dwalter/basketo/BasketoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwalter.basketo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
